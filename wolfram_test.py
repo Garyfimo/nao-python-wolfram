dc0735ff967358ac99670df37f0f7417a5c8fda0
Garyfimo <garyfimo@gmail.com>
2015-02-10 15:22:43 -0500

    uu

--- wolfram_test.py
+++ wolfram_test.py
@@ -1,4 +1,3 @@
-#-*- coding: utf-8 -*-
 from flask import Flask, make_response
 from json import dumps
 import pytest
@@ -8,16 +7,17 @@ app_id = "4J8QQ4-YK54UPKG83"
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+	return "Wolfram test"
+
 @app.route("/ask/<question>")
 def test_basic(question):
 	cliente = wolframalpha.Client(app_id)
-	#res = cliente.query('who is brasil president')
-	res = cliente.query('capital of peru')
+	res = cliente.query(question)
 	if len(res.pods) > 0:
 		results = list(res.results)
-		#for result in results:
-		#	print result.text
-		return results[0].text
+		return make_response(dumps(results[0].text, ensure_ascii=False).encode("utf-8"))
 	else:
 		return "No tengo respuesta para eso"
 
